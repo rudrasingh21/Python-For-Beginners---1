179cd55c99c864e841a200e50ec16f61172c949f
rudrasingh21 <30667041+rudrasingh21@users.noreply.github.com>
2019-01-20 00:37:55 +0530

    Update 05.Lists.py

--- 05.Lists.py
+++ 05.Lists.py
@@ -47,3 +47,27 @@ print(len(items))
 print ("A" in food)
 
 print("x"  in food)
+
+#List Operations 
+
+>>> l=list(range(-6,7,2))
+>>> l
+[-6, -4, -2, 0, 2, 4, 6]
+
+#List Operation
+
+>>> [[x**2,x**3] for x in range(4)]
+[[0, 0], [1, 1], [4, 8], [9, 27]]
+
+---------------
+# List clear() 
+--------------
+
+# Defining a list
+l = [{1, 2}, ('a'), ['1.1', '2.2']]
+
+# clearing the list
+l.clear()
+
+print('List:', l)
+
